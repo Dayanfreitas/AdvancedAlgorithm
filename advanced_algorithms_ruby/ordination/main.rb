39aa0555f126cbefac3fa113645427fd4ac3653e
Dayanfreitas <dayan.freitas@catolicasc.org.br>
2022-08-28 19:35:17 -0300

    bubble sort with test

--- advanced_algorithms_ruby/ordination/main.rb
+++ advanced_algorithms_ruby/ordination/main.rb
@@ -1,8 +1,10 @@
-require_relative "./../ultis/random_list.rb"
-require_relative "./../ultis/vetor.rb"
 require 'benchmark'
 
-list = Vetor.new(RandomList.size(100))
+require_relative "../ordination/bubble_sort/bubble_sort.rb"
+require_relative "./../ultis/random_list.rb"
+require_relative "./../ultis/vetor.rb"
+
+list = Vetor.new(RandomList.size(5))
 
 list_bubble_sort = list.dup
 list_sort = list.dup
@@ -10,14 +12,12 @@ list_sort = list.dup
 p "BEFORE list_bubble_sort => #{list_bubble_sort}"
 p "BEFORE list_sort => #{list_sort}"
 
+def bubble_sort(list)
+    BubbleSort.sort(list)
+end
+
 Benchmark.bmbm do |x|
-    x.report("Bubble Sort") { 
-        for i in (0..list_bubble_sort.length - 1)
-            for j in ((i + 1)..list_bubble_sort.length - 1)
-                list_bubble_sort.swap!(i, j) if list_bubble_sort.is_bigger?(i, j)
-            end
-        end
-     }
+    x.report("Bubble Sort") { bubble_sort(list_bubble_sort) }
     x.report("SORT")  { list_sort.sort!  }
 end
 p "AFTER list_bubble_sort => #{list_bubble_sort}"
--- advanced_algorithms_ruby/test/main.rb
+++ advanced_algorithms_ruby/test/main.rb
@@ -1,1 +1,2 @@
 require "./ultis/vetor_test.rb"
+require "./ordination/bubble_sort/bubble_sort.rb"
